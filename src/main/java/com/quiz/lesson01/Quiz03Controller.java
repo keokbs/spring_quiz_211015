88bdb332149d3cc59104e476ca87d5498491b40b
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-01-18 15:19:32 +0900

    jsp 뷰 연동하기 퀴즈

---
+++ src/main/java/com/quiz/lesson01/Quiz03Controller.java
@@ -0,0 +1,12 @@
+package com.quiz.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Quiz03Controller {
+	@RequestMapping("/lesson01/quiz03/1")
+	public String quiz03_1() {
+		return "/lesson01/quiz03_1";
+	}
+}
