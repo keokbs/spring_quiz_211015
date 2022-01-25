2d3b68e7a668cc431b6c8ed35c376c49c1d3b830
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-01-26 01:07:52 +0900

    keokbs/spring_quiz_211015#5 스프링 설정

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -1,18 +1,10 @@
 package com.quiz;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.context.annotation.ComponentScan;
 
-@Configuration
+@ComponentScan(basePackages = "com.quiz")
 @SpringBootApplication
 public class SpringQuizApplication {
 
--- src/main/java/com/quiz/config/DatabaseConfig.java
+++ src/main/java/com/quiz/config/DatabaseConfig.java
@@ -11,7 +11,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan(basePackages = "com.quiz.*")  // mapper scan 추가!!!
+@MapperScan(basePackages = "com.quiz.*")  // interface(mapper)가 있는 패키지 경로
 public class DatabaseConfig {
 	@Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
