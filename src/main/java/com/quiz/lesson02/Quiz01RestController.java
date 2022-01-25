f2c7c49713194ec7193a8fb355e05cda872c9851
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-01-26 01:08:19 +0900

    lesson02 날린 파일 추가

--- src/main/java/com/quiz/lesson02/Quiz01RestController.java
+++ src/main/java/com/quiz/lesson02/Quiz01RestController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quiz.lesson02.bo.StoreBO;
